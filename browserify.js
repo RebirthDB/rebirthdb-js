2ac8b423bbf0390d9a200177b3d2c58b33736cc3
Andy Burke <andy.burke@mailbox.earth>
2018-06-21 17:01:17 -0700

    rename and reformat

--- browserify.js
+++ browserify.js
@@ -1,35 +1,44 @@
-const b = require('browserify')('./lib')
-const {writeFileSync} = require('fs')
+const b = require( 'browserify' )( './lib' )
+const fs = require( 'fs' )
 
 const REQUIRE_FILES = [
-  'connection.js',
-  'cursor.js',
-  'dequeue.js',
-  'error.js',
-  'helper.js',
-  'linked_list.js',
-  'metadata.js',
-  'pool.js',
-  'pool_master.js',
-  'protodef.js',
-  'stream.js',
-  'term.js',
-  'transform_stream.js',
-  'writable_stream.js'
-]
+    'connection.js',
+    'cursor.js',
+    'dequeue.js',
+    'error.js',
+    'helper.js',
+    'linked_list.js',
+    'metadata.js',
+    'pool.js',
+    'pool_master.js',
+    'protodef.js',
+    'stream.js',
+    'term.js',
+    'transform_stream.js',
+    'writable_stream.js'
+];
 
-b.add('./lib/index.js')
+b.add( './lib/index.js' );
 
-for (const file of REQUIRE_FILES) {
-  b.require('./lib/' + file, {expose: './lib/' + file})
+for ( const file of REQUIRE_FILES ) {
+    b.require( './lib/' + file, {
+        expose: './lib/' + file
+    } );
 }
 
-b.require('./lib/index.js', {expose: 'rethinkdbdash'})
+b.require( './lib/index.js', {
+    expose: 'rebirthdb-js'
+} );
 
-b.bundle(function (err, result) {
-  if (err) {
-    console.error(err)
-  } else {
-    writeFileSync('./rethinkdbdash.js', result)
-  }
-})
+b.bundle( ( err, result ) => {
+    if ( err ) {
+        console.error( err );
+        return;
+    }
+
+    if ( !fs.existsSync( './dist' ) ) {
+        fs.mkdirSync( './dist' );
+    }
+
+    fs.writeFileSync( './dist/rebirthdb-js.js', result );
+} );
